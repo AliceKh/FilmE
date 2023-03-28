1d5a4a44e27dff422551ba9b6e4415a94de4c46c
Rashel Tankilevich <rashel2468@gmail.com>
2023-03-28 15:17:11 +0300

    styling

--- FilmeApp/Navigator.js
+++ FilmeApp/Navigator.js
@@ -1,10 +1,15 @@
 import {createStackNavigator} from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
+import {createAppContainer, } from 'react-navigation';
 import Login from './components/LoginPage';
 import Register from './components/RegistrationPage'
 
 const stackNavigatorOptions = {
-    headerShown:false
+    headerShown:false,
+    cardStyle: {
+        backgroundColor: 'rgba(246, 230, 255, 0.1)'
+        
+    }
+    
 }
 const AppNavigator = createStackNavigator({
     Login:{screen:Login},
--- FilmeApp/components/RegistrationPage.js
+++ FilmeApp/components/RegistrationPage.js
@@ -1,99 +1,117 @@
 import React from 'react';
-import {Text,View,Image, TextInput} from 'react-native';
+import {Text,View,Image, TextInput, Button} from 'react-native';
 
-export default class Register extends React.Component{
+export default class Login extends React.Component{
 
     render(){
-        
+        const {navigate} = this.props.navigation
         return(
-            <View style={{backgroundColor:"#FFF",height:"100%"}}>
-                <Text
-                 style={{
-                     fontSize:30,
-                     alignSelf:"center",
-                 }}
-                >FilmE</Text>
-
-                <View style={{
-                    flexDirection:"row",
-                    alignItems:"center",
-                    marginHorizontal:55,
-                    borderWidth:2,
-                    marginTop:50,
-                    paddingHorizontal:10,
-                    borderColor:"#00716F",
-                    borderRadius:23,
-                    paddingVertical:2
-                }}>
-                  
-                    <TextInput 
-                        placeholder="Email"
-                        placeholderTextColor="#00716F"
-                        style={{paddingHorizontal:10}}
-                    />
-
-                    
-
-                </View>
-                <View style={{
-                    flexDirection:"row",
-                    alignItems:"center",
-                    marginHorizontal:55,
-                    borderWidth:2,
-                    marginTop:15,
-                    paddingHorizontal:10,
-                    borderColor:"#00716F",
-                    borderRadius:23,
-                    paddingVertical:2
-                }}>
-                   
-                   <TextInput 
-                        secureTextEntry
-                        placeholder="Password"
-                        placeholderTextColor="#00716F"
-                        style={{paddingHorizontal:10}}
-                    />
-
-
-                    
-
-                </View>
-                <View style={{
-                    flexDirection:"row",
-                    alignItems:"center",
-                    marginHorizontal:55,
-                    borderWidth:2,
-                    marginTop:15,
-                    paddingHorizontal:10,
-                    borderColor:"#00716F",
-                    borderRadius:23,
-                    paddingVertical:2
-                }}>
-                   
-                   <TextInput 
-                        secureTextEntry
-                        placeholder="Confirm Password"
-                        placeholderTextColor="#00716F"
-                        style={{paddingHorizontal:10}}
-                    />
-                    
-
-                </View>
-
+            <View style={{height:"100%"}}>
                 <View style={{
                     marginHorizontal:55,
                     alignItems:"center",
                     justifyContent:"center",
-                    marginTop:30,
-                    backgroundColor:"#00716F",
+                    marginTop:45,
                     paddingVertical:10,
                     borderRadius:23
                 }}>
                     <Text style={{
-                        color:"white",
-                    }}>Register</Text>
+                        color:"#9960D2",
+                        margin:5,
+                        fontSize:24
+                    }}>Welcome!</Text>
                 </View>
-              
+                <View style={{
+                    flexDirection:"row",
+                    alignItems:"center",
+                    marginHorizontal:55,
+                    borderWidth:2,
+                    marginTop:55,
+                    paddingHorizontal:10,
+                    borderColor:"#9960D2",
+                    borderRadius:12,
+                    paddingVertical:2
+                }}>
+                    <TextInput 
+                        placeholder="Email"
+                        placeholderTextColor="#909580"
+                        textAlign='left'
+                        style={{width:"100%"}}
+                    />
+                </View>
+
+                <View style={{
+                    flexDirection:"row",
+                    alignItems:"center",
+                    marginHorizontal:55,
+                    borderWidth:2,
+                    marginTop:25,
+                    paddingHorizontal:10,
+                    borderColor:"#9960D2",
+                    borderRadius:12,
+                    paddingVertical:2
+                }}>
+                    <TextInput 
+                        placeholder="Password"
+                        placeholderTextColor="#909580"
+                        secureTextEntry={true}
+                        color='#6E2E76'
+                        textAlign='left'
+                        style={{width:"100%"}}
+                    />
+                </View>
+
+                <View style={{
+                    flexDirection:"row",
+                    alignItems:"center",
+                    marginHorizontal:55,
+                    borderWidth:2,
+                    marginTop:25,
+                    paddingHorizontal:10,
+                    borderColor:"#9960D2",
+                    borderRadius:12,
+                    paddingVertical:2
+                }}>
+                    <TextInput 
+                        placeholder="Confirm Password"
+                        placeholderTextColor="#909580"
+                        secureTextEntry={true}
+                        color='#6E2E76'
+                        textAlign='left'
+                        style={{width:"100%"}}
+                    />
+                </View>
+
+                <View style={{
+                    marginHorizontal:55,
+                    paddingHorizontal:10,
+                    alignItems:"center",
+                    justifyContent:"center",
+                    marginTop:50,
+                    paddingVertical:10
+                }}>
+                    <Button title='Sign Up'
+                        color="#9960D2"
+                        ></Button>
+                </View>
+
+                <Image
+                    style={{alignSelf:"center",
+                            margin:"10%",
+                            height:240,
+                        width:135}}
+                            
+                    source={require("../assets/logo1.jpg")}
+                />
+                <Text                 
+                    onPress={()=>navigate('Login')}
+                    
+                    style={{
+                        alignSelf:"center",
+                        color:"#9960D2",
+                        paddingVertical:10
+                    }}>Login
+                </Text>
             </View>
         )
     }
