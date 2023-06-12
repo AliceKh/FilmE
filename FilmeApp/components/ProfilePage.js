94fb270c7973acd251feaeab0463828ca55ef8a9
Dan <dgmnsn@gmail.com>
2023-06-12 21:50:44 +0300

    Showing a dynamic graphs according to timestamps. Working with mock data

--- FilmeApp/components/ProfilePage.js
+++ FilmeApp/components/ProfilePage.js
@@ -1,85 +1,118 @@
 import React from 'react';
-import { View, Modal, Text, Image, StyleSheet, FlatList, TouchableOpacity, Dimensions} from 'react-native';
+import {
+  View,
+  Modal,
+  Text,
+  Image,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 import axios from 'axios';
+import GraphPage from './GraphPage';
 
 export default class ProfileScreen extends React.Component {
-    constructor(props) {
-        super(props);
-        this.navigate = props.navigation
-        this.state = {
-          selectedImageId: null,
-          isMenuVisible: false,
-          showList: true,
-          user: null,
-          songs: [], 
-        };
-        this.toggleList = this.toggleList.bind(this);
-      }
+  constructor(props) {
+    super(props);
+    this.navigate = props.navigation;
+    this.state = {
+      selectedImageId: null,
+      isMenuVisible: false,
+      showList: true,
+      user: null,
+      songs: [],
+      isGraphVisible: false, // Added state for graph visibility
+    };
+    this.toggleList = this.toggleList.bind(this);
+  }
 
-      componentDidMount() {
-        const userId = '644d2ec5ccb302c74d5d91b2';
-        axios.get(`http://localhost:4000/user/${userId}`)
-          .then(response => {
-            this.setState({ user: response.data });
-          })
-          .catch(error => {
-            console.log(error);
-          });
-          
-        axios.get('http://localhost:4000/uploads')
-          .then(response => {
-            this.setState({ songs: response.data });
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }
+  componentDidMount() {
+    const userId = '644d2ec5ccb302c74d5d91b2';
+    axios
+      .get(`http://localhost:4000/user/${userId}`)
+      .then((response) => {
+        this.setState({ user: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-      renderItem = ({ item, index }) => {
-        const column = index % 3;
-        const itemWidth = Dimensions.get('window').width / 3 - 12;
-      
-        return (
-          <TouchableOpacity
-            style={[styles.itemContainer, column === 0 && styles.itemContainerFirstColumn, item.id === this.state.selectedImageId && styles.selectedItemContainer]}
-            onPress={() => {
-              this.setState({ selectedImageId: item.id }); 
-              this.props.navigation.navigate('GraphPage');
-            }}
-          >
-            <Image style={[styles.itemImage, { width: itemWidth }]} source={{ uri: item.LinkToPreviewImage }} />
-          </TouchableOpacity>
-        );
-      }      
+    axios
+      .get('http://localhost:4000/uploads')
+      .then((response) => {
+        this.setState({ songs: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    toggleMenu = () => {
-        this.setState({isMenuVisible: !this.state.isMenuVisible})
-    }
+  renderItem = ({ item, index }) => {
+    const column = index % 3;
+    const itemWidth = Dimensions.get('window').width / 3 - 12;
 
-    toggleList = () => {
-      this.setState(prevState => ({
-        showList: !prevState.showList,
-      }));
-    }
+    return (
+      <TouchableOpacity
+        style={[
+          styles.itemContainer,
+          column === 0 && styles.itemContainerFirstColumn,
+          item.id === this.state.selectedImageId && styles.selectedItemContainer,
+        ]}
+        onPress={() => {
+          this.setState({ selectedImageId: item.id, isGraphVisible: true }); // Show graph on block click
+        }}
+      >
+        <Image style={[styles.itemImage, { width: itemWidth }]} source={{ uri: item.LinkToPreviewImage }} />
+      </TouchableOpacity>
+    );
+  };
 
-    renderMenu = () => {
-        return (
-          <Modal visible={this.state.isMenuVisible} transparent animationType="none">
-            <TouchableOpacity style={{ flex: 1 }} onPress={this.toggleMenu}>
-              <View style={{ flex: 1}}>
-                <View style={{ position: 'absolute', top: 50, right: 2, width: 150, borderRadius: 8, padding: 16, backgroundColor: '#5e0362a3' }}>
-                  <TouchableOpacity style={{ marginBottom: 8 }}>
-                    <Text style={{ fontSize: 16, color: 'white',textAlign: 'center' }}>Settings</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Text style={{ fontSize: 16, color: 'white',textAlign: 'center' }}>Help</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </TouchableOpacity>
-          </Modal>
-        );
-    }
+  toggleMenu = () => {
+    this.setState({ isMenuVisible: !this.state.isMenuVisible });
+  };
+
+  toggleList = () => {
+    this.setState((prevState) => ({
+      showList: !prevState.showList,
+    }));
+  };
+
+  renderMenu = () => {
+    return (
+      <Modal visible={this.state.isMenuVisible} transparent animationType="none">
+        <TouchableOpacity style={{ flex: 1 }} onPress={this.toggleMenu}>
+          <View style={{ flex: 1 }}>
+            <View style={{ position: 'absolute', top: 50, right: 2, width: 150, borderRadius: 8, padding: 16, backgroundColor: '#5e0362a3' }}>
+              <TouchableOpacity style={{ marginBottom: 8 }}>
+                <Text style={{ fontSize: 16, color: 'white', textAlign: 'center' }}>Settings</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style={{ fontSize: 16, color: 'white', textAlign: 'center' }}>Help</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </Modal>
+    );
+  };
+
+  renderGraphModal = () => {
+    return (
+      <Modal visible={this.state.isGraphVisible} transparent animationType="slide">
+        <TouchableOpacity
+          style={styles.modalBackground}
+          activeOpacity={1} // Prevents TouchableOpacity from handling touch events
+          onPress={() => this.setState({ isGraphVisible: false })}
+        >
+          <View style={styles.modalContent}>
+            <GraphPage />
+          </View>
+        </TouchableOpacity>
+      </Modal>
+    );
+  };
+
 
   render() {
     const { showList, user } = this.state;
@@ -87,12 +120,12 @@ export default class ProfileScreen extends React.Component {
       <View style={styles.body}>
         {/* Header section */}
         <View style={styles.header}>
-            <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-                <Image source={require('../images/previous.png')} 
-                       style={{ width: 20, height: 20, color: 'white' }} />
-            </TouchableOpacity>  
+          <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
+            <Image source={require('../images/previous.png')} style={{ width: 20, height: 20, color: 'white' }} />
+          </TouchableOpacity>
           <TouchableOpacity onPress={() => this.props.navigation.navigate('ExplorePage')}>
-            <Text style={ styles.headerText }>{"  Explore Page "}
+            <Text style={styles.headerText}>
+              {"  Explore Page "}
               <Image source={require('../images/up.png')} style={{ width: 16, height: 16 }} />
             </Text>
           </TouchableOpacity>
@@ -102,35 +135,36 @@ export default class ProfileScreen extends React.Component {
         </View>
 
         {this.renderMenu()}
+        {this.renderGraphModal()}
 
-       {/* Profile picture section */}
-       <View style={{ alignItems: 'center', marginTop: 16 }}>
-          <Image source={user && {uri : user.ProfileImage}} style={{ width: 120, height: 120, borderRadius: 60 }} />
-          <Text style={ styles.profileName }>{user && user.Username}</Text>
+        {/* Profile picture section */}
+        <View style={{ alignItems: 'center', marginTop: 16 }}>
+          <Image source={user && { uri: user.ProfileImage }} style={{ width: 120, height: 120, borderRadius: 60 }} />
+          <Text style={styles.profileName}>{user && user.Username}</Text>
         </View>
 
         {/* Followers, following, likes row */}
-        <View style={[styles.centerStyle, {marginTop: 16 }]}>
-          <View style={[{ alignItems: 'center' },{marginRight: 8}]}>
+        <View style={[styles.centerStyle, { marginTop: 16 }]}>
+          <View style={[{ alignItems: 'center' }, { marginRight: 8 }]}>
             <Text style={styles.infoStatic}>{user && user.NumberOfFollowing}</Text>
-            <Text style={ styles.infoName }>Following</Text>
+            <Text style={styles.infoName}>Following</Text>
           </View>
-          <View style={[{ alignItems: 'center' },{marginRight: 8}]}>
+          <View style={[{ alignItems: 'center' }, { marginRight: 8 }]}>
             <Text style={styles.infoStatic}>{user && user.NumberOfFollowers}</Text>
-            <Text style={ styles.infoName }>Followers</Text>
+            <Text style={styles.infoName}>Followers</Text>
           </View>
           <View style={{ alignItems: 'center' }}>
             <Text style={styles.infoStatic}>{user && user.NumberOfReactions}</Text>
-            <Text style={ styles.infoName }>Reactions</Text>
+            <Text style={styles.infoName}>Reactions</Text>
           </View>
         </View>
 
         {/* Buttons section */}
-        <View style={[styles.centerStyle, {marginVertical: 16, marginBottom: 20 }]}>
-          <TouchableOpacity style={[styles.followBtn,{marginRight: 8}]}>
-            <Text style={ styles.headerText }>Follow</Text>
+        <View style={[styles.centerStyle, { marginVertical: 16, marginBottom: 20 }]}>
+          <TouchableOpacity style={[styles.followBtn, { marginRight: 8 }]}>
+            <Text style={styles.headerText}>Follow</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.iconBtn,{marginRight: 8}]}>
+          <TouchableOpacity style={[styles.iconBtn, { marginRight: 8 }]}>
             <Image source={require('../images/spotify.png')} style={{ width: 30, height: 30 }} />
           </TouchableOpacity>
           <TouchableOpacity style={styles.iconBtn}>
@@ -138,10 +172,10 @@ export default class ProfileScreen extends React.Component {
           </TouchableOpacity>
         </View>
         <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
-          <TouchableOpacity onPress={() => this.setState({showList : true})}>
+          <TouchableOpacity onPress={() => this.setState({ showList: true })}>
             <Image source={require('../images/grid.png')} style={{ width: 30, height: 30 }} />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.setState({showList : false})} >
+          <TouchableOpacity onPress={() => this.setState({ showList: false })}>
             <Image source={require('../images/alert.png')} style={{ width: 30, height: 30 }} />
           </TouchableOpacity>
         </View>
@@ -149,86 +183,103 @@ export default class ProfileScreen extends React.Component {
         {/* Video list section */}
         {showList ? (
           <View style={styles.container}>
-            <FlatList
-              data={this.state.songs}
-              renderItem={this.renderItem}
-              keyExtractor={item => item.id}
-              numColumns={3}
-            />
+            <FlatList data={this.state.songs} renderItem={this.renderItem} keyExtractor={(item) => item.id} numColumns={3} />
           </View>
         ) : (
           <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{color: 'white'}}>Notifications</Text>
+            <Text style={{ color: 'white' }}>Notifications</Text>
           </View>
         )}
-    </View>
-);
-    }
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    header:{
-        flexDirection: 'row',
-        alignItems: 'center', 
-        justifyContent: 'space-between', 
-        padding: 16
-    },
-    body:{
-        flex: 1,
-        backgroundImage: 'linear-gradient(to right, #29024f, #000000, #29024f)',
-    },
-    container: {
-      flex: 1,
-      padding: 16,
-    },
-    itemContainer: {
-      flex: 1,
-      margin: 4,
-      borderRadius: 2,
-      overflow: 'hidden',
-      alignItems: 'center'
-    },
-    selectedItemContainer: {
-      borderWidth: 2,
-      borderColor: '#2196f3',
-    },
-    itemImage: {
-      flex: 1,
-      width: 210,
-      height: 235,
-      aspectRatio: 1,
-    },
-    infoName:{
-        fontSize: 16,
-        color: 'gray'
-    },
-    infoStatic:{
-        fontWeight: 'bold',
-        fontSize: 14,
-        color: 'white'
-    },
-    profileName:{
-        fontWeight: 'bold',
-        fontSize: 18,
-        marginTop: 8,
-        color: 'white',
-    },
-    headerText:{
-        fontSize: 16,
-        color: 'white'
-    },
-    followBtn:{
-        backgroundColor: 'red',
-        paddingVertical: 8,
-        paddingHorizontal: 16, 
-        borderRadius: 20
-    },
-    centerStyle:{
-        flexDirection: 'row', 
-        justifyContent: 'center', 
-        alignItems: 'center'
-    },
-    iconBtn:{
-        borderColor: '#686060', borderWidth: 1, borderRadius: 3
-    },
-  });
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 16,
+  },
+  body: {
+    flex: 1,
+    backgroundImage: 'linear-gradient(to right, #29024f, #000000, #29024f)',
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  itemContainer: {
+    flex: 1,
+    margin: 4,
+    borderRadius: 2,
+    overflow: 'hidden',
+    alignItems: 'center',
+  },
+  selectedItemContainer: {
+    borderWidth: 2,
+    borderColor: '#2196f3',
+  },
+  itemImage: {
+    flex: 1,
+    width: 210,
+    height: 235,
+    aspectRatio: 1,
+  },
+  infoName: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  infoStatic: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: 'white',
+  },
+  profileName: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 8,
+    color: 'white',
+  },
+  headerText: {
+    fontSize: 16,
+    color: 'white',
+  },
+  followBtn: {
+    backgroundColor: 'red',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 20,
+  },
+  centerStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconBtn: {
+    borderColor: '#686060',
+    borderWidth: 1,
+    borderRadius: 3,
+  },
+  graphContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 16,
+  },
+  graph: {
+    borderRadius: 16,
+  },
+  modalContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalBackground: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
