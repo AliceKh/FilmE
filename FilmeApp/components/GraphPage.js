3f1ebc5a322437520f2fa147c8835b13b11c7bbe
Dan <dgmnsn@gmail.com>
2023-06-15 21:09:16 +0300

    finish

--- FilmeApp/components/GraphPage.js
+++ FilmeApp/components/GraphPage.js
@@ -1,30 +1,71 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
 
 export default class GraphPage extends React.Component {
+  render() {
+    const data = {
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+      datasets: [
+        {
+          data: [20, 45, 28, 80, 99],
+        },
+      ],
+    };
 
-    render() {
-        return (
-            <View style={{backgroundColor: "#FFF", height: "100%"}}>
+    return (
+      <View style={styles.body}>
+        {/* Header section */}
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Graph Page</Text>
+        </View>
 
-                <View style={{
-                    marginHorizontal: 20,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    marginTop: 30,
-                    paddingVertical: 10,
-                    borderRadius: 23
-                }}>
-                    <Image
-                        source={require('../assets/graph.jpg')}
-                        style={{
-                            width: '100%',
-                            height: undefined,
-                            aspectRatio: 1
-                        }}/>
-                </View>
-
-            </View>
-        )
-    }
+        {/* Graph section */}
+        <View style={styles.graphContainer}>
+          <BarChart
+            data={data}
+            width={300}
+            height={200}
+            yAxisLabel=""
+            chartConfig={{
+              backgroundGradientFrom: '#1E2923',
+              backgroundGradientTo: '#08130D',
+              fillShadowGradientOpacity: 1,
+              color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+            }}
+            style={styles.graph}
+          />
+        </View>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  body: {
+    flex: 1,
+    backgroundImage: 'linear-gradient(to right, #29024f, #000000, #29024f)',
+  },
+  headerText: {
+    fontSize: 16,
+    color: 'white',
+  },
+  graphContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 16,
+  },
+  graph: {
+    borderRadius: 16,
+  },
+});
