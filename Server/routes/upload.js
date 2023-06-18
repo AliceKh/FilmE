0ded8569d39947282ce394a5d78b16c6712c237b
Daniel Akulich <daniakuli2@gmail.com>
2023-06-18 13:03:01 +0300

    made some changes with the Upload content

--- Server/routes/upload.js
+++ Server/routes/upload.js
@@ -3,6 +3,8 @@ import {insertUpload} from "../controllers/db/upload.js";
 import {uploadVideoMulter} from "../controllers/firebaseStorageUpload/uploadVideo.js";
 import {uploadAudioMulter} from "../controllers/firebaseStorageUpload/uploadAudio.js";
 import {uploadPreviewMulter} from "../controllers/firebaseStorageUpload/uploadPreview.js";
+import { bucketName, firebaseInstance, storage, auth } from '../firebaseUtils.js';
+import { ref, uploadBytes } from 'firebase/storage';
 
 const router = express.Router();
 
@@ -22,10 +24,34 @@ router.post('/', function (req, res) {
     res.send('respond with a resource');
 });
 
-router.route('/video')
-    .post(uploadVideoMulter.single('file'), (req, res) => {
-        res.status(201).json(req.file)
-    });
+// router.route('/video')
+//     .post(uploadVideoMulter.single('file'), (req, res) => {
+//         res.status(201).json(req.file)
+//     });
+
+router.route('/video').post(async (req, res) => {
+    try {
+        const { uri, name, type } = req.body;
+    
+        const currentUser = auth.currentUser;
+    
+        if (currentUser) {
+          const storageRef = firebaseInstance.storage().bucket().file(`video/${name}`);
+          const snapshot = await storageRef.save(uri, { contentType: 'video/mp4' });
+    
+          // Get the download URL using getSignedUrl() method
+          const downloadURL = `https://firebasestorage.googleapis.com/v0/b/${storageRef.bucket.name}/o/${encodeURIComponent(storageRef.name)}?alt=media`;
+    
+          res.json({ downloadURL });
+        } else {
+          res.status(401).json({ error: 'User is not authenticated' });
+        }
+      } catch (error) {
+        console.error('Error uploading file:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+  });
+    
 router.route('/audio')
     .post(uploadAudioMulter.single('file'), (req, res) => {
         res.status(201).json(req.file)
