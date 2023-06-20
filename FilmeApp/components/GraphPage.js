88ed41d78a8560d719840e5b02af0714d6b45182
Dan <dgmnsn@gmail.com>
2023-06-20 14:41:11 +0300

    grapg page close button

--- FilmeApp/components/GraphPage.js
+++ FilmeApp/components/GraphPage.js
@@ -8,82 +8,29 @@ export default class GraphPage extends React.Component {
     super(props);
     this.state = {
       activeDatasetIndex: 0,
-      datasets: [
-        {
-          timestamp: '00:01:42',
-          totalReactions: 4,
-          emotionAvg: {
-            joyAvg: 4.5,
-            sorrowAvg: 1.5,
-            angerAvg: 3.5,
-            surpriseAvg: 2.5,
-          },
-        },
-        {
-          timestamp: '00:02:33',
-          totalReactions: 4,
-          emotionAvg: {
-            joyAvg: 2.5,
-            sorrowAvg: 3.5,
-            angerAvg: 1.5,
-            surpriseAvg: 3,
-          },
-        },
-        {
-          timestamp: '00:03:55',
-          totalReactions: 2,
-          emotionAvg: {
-            joyAvg: 3,
-            sorrowAvg: 4,
-            angerAvg: 2,
-            surpriseAvg: 1,
-          },
-        },        {
-          timestamp: '00:03:55',
-          totalReactions: 2,
-          emotionAvg: {
-            joyAvg: 3,
-            sorrowAvg: 4,
-            angerAvg: 2,
-            surpriseAvg: 1,
-          },
-        },        {
-          timestamp: '00:03:55',
-          totalReactions: 2,
-          emotionAvg: {
-            joyAvg: 3,
-            sorrowAvg: 4,
-            angerAvg: 2,
-            surpriseAvg: 1,
-          },
-        },
-      ],
+      datasets: [],
       contentObjectID: null,
+      isLoading: true,
     };
   }
 
   componentDidMount() {
     const { objectId } = this.props;
-    console.log('Received objectId in GraphPage:', objectId);
     this.setState({ contentObjectID: objectId }, () => {
       this.fetchAnalyticsData();
     });
   }
 
   fetchAnalyticsData = () => {
-    // const contentObjectID = props.objectId;
-    // console.log(this.state);
-    console.log("contentObjectID: " + this.state.contentObjectID);
     axios
-      .get(`http://${global.server}:4000/analytics/` + this.state.contentObjectID, {
-      })
+      .get(`http://192.168.1.247:4000/analytics/${this.state.contentObjectID}`)
       .then(response => {
         const { reactions } = response.data;
-        console.log(reactions);
-        this.setState({ datasets: reactions });
+        this.setState({ datasets: reactions, isLoading: false });
       })
       .catch(error => {
         console.error('Error fetching analytics data:', error);
+        this.setState({ isLoading: false });
       });
   };
 
@@ -92,7 +39,7 @@ export default class GraphPage extends React.Component {
   };
 
   render() {
-    const { activeDatasetIndex, datasets } = this.state;
+    const { activeDatasetIndex, datasets, isLoading } = this.state;
 
     const activeDataset = datasets[activeDatasetIndex];
 
@@ -113,16 +60,16 @@ export default class GraphPage extends React.Component {
         labelCount: 6,
       },
     };
-    
+
     const chartData = {
       labels: ['Joy', 'Sorrow', 'Anger', 'Surprise'],
       datasets: [
         {
           data: [
-            activeDataset.emotionAvg.joyAvg,
-            activeDataset.emotionAvg.sorrowAvg,
-            activeDataset.emotionAvg.angerAvg,
-            activeDataset.emotionAvg.surpriseAvg,
+            activeDataset ? activeDataset.emotionAvg.joyAvg : 0,
+            activeDataset ? activeDataset.emotionAvg.sorrowAvg : 0,
+            activeDataset ? activeDataset.emotionAvg.angerAvg : 0,
+            activeDataset ? activeDataset.emotionAvg.surpriseAvg : 0,
           ],
         },
       ],
@@ -130,36 +77,51 @@ export default class GraphPage extends React.Component {
 
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Graph Title</Text>
-
         <Modal animationType="slide" transparent={true} visible={true}>
           <View style={styles.popupContainer}>
-            <View style={styles.chartContainer}>
-              <BarChart
-                data={chartData}
-                width={Dimensions.get('window').width * 0.8}
-                height={200}
-                chartConfig={chartConfig}
-                style={styles.chartStyle}
-              />
-              <Text style={styles.timestamp}>{activeDataset.timestamp}</Text>
-            </View>
+            {datasets.length === 0 ? (
+              <View style={styles.chartContainer}>
+                <View style={styles.noDataContainer}>
+                  <Text style={styles.noDataText}>No analytics data available</Text>
+                </View>
+              </View>
+            ) : (
+              <View style={styles.chartContainer}>
+                <BarChart
+                  data={chartData}
+                  width={Dimensions.get('window').width * 0.8}
+                  height={200}
+                  chartConfig={chartConfig}
+                  style={styles.chartStyle}
+                />
+              </View>
+            )}
 
-            <Text style={styles.timestampTitle}>Timestamps:</Text>
+            {datasets.length > 0 && (
+              <View style={styles.timestampContainer}>
+                <Text style={styles.timestampTitle}>Timestamps:</Text>
 
-            <View style={styles.buttonContainer}>
-              {datasets.map((dataset, index) => (
-                <TouchableOpacity
-                  key={index}
-                  style={[
-                    styles.button,
-                    index === activeDatasetIndex && styles.activeButton,
-                  ]}
-                  onPress={() => this.handleClick(index)}
-                >
-                  <Text style={styles.buttonText}>{dataset.timestamp}</Text>
-                </TouchableOpacity>
-              ))}
+                <View style={styles.buttonContainer}>
+                  {datasets.map((dataset, index) => (
+                    <TouchableOpacity
+                      key={index}
+                      style={[
+                        styles.button,
+                        index === activeDatasetIndex && styles.activeButton,
+                      ]}
+                      onPress={() => this.handleClick(index)}
+                    >
+                      <Text style={styles.buttonText}>{dataset.timestamp}</Text>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+              </View>
+            )}
+
+            <View style={styles.closeButtonContainer} onPress={console.log(this.props.closeGraph)}>
+              <TouchableOpacity style={styles.closeButton}>
+                <Text style={styles.closeButtonText} >Close</Text>
+              </TouchableOpacity>
             </View>
           </View>
         </Modal>
@@ -184,6 +146,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  timestampContainer: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    padding: 16,
+    marginBottom: 16,
   },
   timestampTitle: {
     fontSize: 16,
@@ -223,5 +191,29 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: 'bold',
     color: 'black',
+  },
+  noDataContainer: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    padding: 16,
+  },
+  noDataText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  closeButtonContainer: {
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  closeButton: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  closeButtonText: {
+    color: 'black',
+    fontWeight: 'bold',
   },
 });
