f7032f61bc31a714b6ae55ee067556834a458650
Alice Khodorkovsky <alish1999@gmail.com>
2023-08-04 20:14:02 +0300

    localStorage localStorage is the browser's database. The data is stored inside your browser in your computer's memory. localStorage is specific to an origin. In other words, the localStorage for one website cannot be accessed by another.

--- FilmeApp/services/StayLoggedInService.js
+++ FilmeApp/services/StayLoggedInService.js
@@ -1,16 +1,14 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
 const storageName = {
     email: "userEmail", password: "userPass"
 }
 export async function saveLogin(email, password) {
-    await AsyncStorage.setItem(storageName.email, password).then();
-    await AsyncStorage.setItem(storageName.password, email).then();
+    await localStorage.setItem(storageName.email, password).then();
+    await localStorage.setItem(storageName.password, email).then();
 }
 
 export function getLogin(setEmail, setPassword) {
     try {
-        Promise.all([AsyncStorage.getItem(storageName.email), AsyncStorage.getItem(storageName.password)]).then(value => {
+        Promise.all([localStorage.getItem(storageName.email), localStorage.getItem(storageName.password)]).then(value => {
             setEmail(value[1]);
             setPassword(value[0]);
             console.log(value);
